ff99f9d81c25e4940a550bfa2dca27be2a94aa80
Ushio <jylzs369@163.com>
2020-06-20 14:34:31 +0800

    add question component

---
+++ src/components/question/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './index.css';
+import { formatWhole, formatSubject, formatTesting } from '../../utils';
+
+import QuestionWhole from './whole';
+import QuestionSubject from './subject';
+import QuestionTesting from './testing';
+
+import questionsOrigin from '../../data/psycology_question.json';
+import answersOrigin from '../../data/psycology_answer.json';
+
+function Question (props) {
+  const { type, subjectId } = props
+  let questions = []
+  let answers = []
+
+  function QuestionComponent () {
+    if (type === 'whole') {
+      questions = formatWhole(questionsOrigin)
+      answers = formatWhole(answersOrigin)
+      return <QuestionWhole questions={questions} answers={answers} />;
+    }
+    if (type === 'subject') {
+      questions = formatSubject(questionsOrigin, subjectId)
+      answers = formatWhole(answersOrigin, subjectId)
+      return <QuestionSubject questions={questions} answers={answers} />;
+    }
+    if (type === 'testing') {
+      questions = formatTesting(questionsOrigin)
+      answers = formatWhole(answersOrigin)
+      console.log(questions)
+      return <QuestionTesting questions={questions} answers={answers} />;
+    }
+  }
+
+  return (
+    <section className="question">
+      <QuestionComponent />
+    </section>
+  );
+}
+
+export default Question;
