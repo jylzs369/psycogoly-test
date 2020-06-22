ff3dca102c93708b9a725ed85d878808e031a0f6
Ushio <jylzs369@163.com>
2020-06-22 09:53:24 +0800

    modify layout

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -9,11 +9,13 @@ function Home () {
         <h1 className="home__title">中科院心理咨询基础培训</h1>
         <h2 className="home__subtitle">（2020年8月题库）</h2>
       </header>
-      <ul className="home__list">
-        <li className="home__list__item"><Link to="/whole">全部试题</Link></li>
-        <li className="home__list__item"><Link to="/subject">单元测试</Link></li>
-        <li className="home__list__item"><Link to="/testing">模拟测试</Link></li>
-      </ul>
+      <main>
+        <ul className="home__list">
+          <li className="home__list__item"><Link to="/whole">全部试题</Link></li>
+          <li className="home__list__item"><Link to="/subject">单元测试</Link></li>
+          <li className="home__list__item"><Link to="/testing">模拟测试</Link></li>
+        </ul>
+      </main>
     </section>
   );
 }
