e88badc3e4a66db72bdb787f0ba1aa6baee14049
Ushio <jylzs369@163.com>
2020-07-01 11:50:38 +0800

    add pm2 config file

---
+++ ecosystem.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  apps : [{
+    name: 'Psycology Test',
+    script: './server/app.js',
+    watch: '.',
+    env: {
+      'COMMON_VARIABLE': true
+    },
+    env_production: {
+      NODE_ENV: 'production '
+    }
+  }/* , {
+    script: './service-worker/',
+    watch: ['./service-worker']
+  } */],
+
+  deploy : {
+    production : {
+      user : 'ushio',
+      host : '106.53.51.186',
+      port: 52001,
+      ref  : 'origin/master',
+      repo : 'git@github.com:jylzs369/psycology-test.git',
+      path : '/www/psycology-test/production',
+      ssh_options: 'StrictHostKeyChecking=no',
+      env: {
+        NODE_ENV: 'production'
+      },
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};
